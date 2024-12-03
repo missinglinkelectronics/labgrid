4767de9c70301a2f1e9c0b5090a05215e8f991d8
Tobias Binkowski <tobias.binkowski@missinglinkelectronics.com>
2024-12-03 17:16:19 +0100

    add AMTPowerPort and AMTPowerDriver to control AMT powered devices
    This currently uses amtctrl as the backend as it is simpler then
    importing the library of the same tool. If more flexibility is needed
    the python amt library could be used.

--- labgrid/remote/client.py
+++ labgrid/remote/client.py
@@ -744,8 +744,7 @@ class ClientSession(ApplicationSession):
         target = self._get_target(place)
         from ..resource.power import NetworkPowerPort, PDUDaemonPort
         from ..resource.remote import NetworkUSBPowerPort, NetworkSiSPMPowerPort
-        from ..resource import TasmotaPowerPort, NetworkYKUSHPowerPort
-
+        from ..resource import TasmotaPowerPort, NetworkYKUSHPowerPort, AMTPowerPort
         drv = None
         try:
             drv = target.get_driver("PowerProtocol", name=name)
@@ -765,6 +764,8 @@ class ClientSession(ApplicationSession):
                     drv = self._get_driver_or_new(target, "TasmotaPowerDriver", name=name)
                 elif isinstance(resource, NetworkYKUSHPowerPort):
                     drv = self._get_driver_or_new(target, "YKUSHPowerDriver", name=name)
+                elif isinstance(resource, AMTPowerPort):
+                    drv = self._get_driver_or_new(target, "AMTPowerDriver", name=name)
                 if drv:
                     break
 
